23481cfb19b2bdd5a58a92eba7cd18458c14cfa9
yasscode <yelka5626@outlook.fr>
2025-08-22 19:18:48 +0200

    réussit de du démarage

--- atherion.os.bat
+++ atherion.os.bat
@@ -1,6 +1,17 @@
 @echo off
 cd /d "%~dp0"
+
+echo Mise à jour du dépôt Git...
 git add .
-git commit -m "Ajout/Mise à jour des fichiers AETHERION OS"
+
+echo.
+set /p commitMessage="Entrez le message du commit : "
+git commit -m "%commitMessage%"
+
+echo.
+echo Envoi sur GitHub...
 git push origin main --force
+
+echo.
+echo ✅ Mise à jour terminée !
 pause
